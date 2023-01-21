7cb7a8106a336c053c16f6b11a43f724a549fe36
Sebastian Sainterling <S.Sainterling@outlook.com>
2023-01-21 13:13:36 -0500

    Added BasicProgramming.java

---
+++ source/BasicProgramming.java
@@ -0,0 +1,17 @@
+package source;
+import java.util.Scanner;
+
+public class BasicProgramming {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num = reader.nextInt();
+
+        if (num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+    }
+} // end class BasicProgramming
